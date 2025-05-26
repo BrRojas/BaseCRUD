6f34c59c6886d206e548aa4ae0cedbceb624ab90
Braian Rojas <braianrojas95@gmail.com>
2025-05-25 23:58:03 -0300

    Agrega constructor y metodos a Product model

--- src/main/java/com/base/basecrud/model/Product.java
+++ src/main/java/com/base/basecrud/model/Product.java
@@ -1,4 +1,65 @@
 package com.base.basecrud.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders = new ArrayList<>();
+
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, List<Order> orders) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.orders = orders;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
