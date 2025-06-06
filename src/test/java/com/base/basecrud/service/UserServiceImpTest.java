52fc8908f08820367eac240312c26175d3d1c135
Braian Rojas <braianrojas95@gmail.com>
2025-06-06 11:25:14 -0300

    test: Se agregó prueba unitaria para el método findAllUser y deleteUserById.

--- src/test/java/com/base/basecrud/service/UserServiceImpTest.java
+++ src/test/java/com/base/basecrud/service/UserServiceImpTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -68,9 +69,36 @@ class UserServiceImpTest {
 
     @Test
     void findAllUsers() {
+
+        List<User> users = List.of(
+                new User(1L,"Braian","1234",null),
+                new User(2L,"Bastian", "1234",null)
+        );
+
+
+        when(userRepository.findAll()).thenReturn(users);
+
+        List<User> result = userServiceImp.findAllUsers();
+
+        assertEquals(2, result.size());
+        assertEquals("Braian", result.get(0).getMail());
+        assertEquals("Bastian", result.get(1).getMail());
+        assertEquals("1234", result.get(0).getPassword());
+        assertEquals("1234", result.get(1).getPassword());
+        verify(userRepository).findAll();
+
     }
 
     @Test
     void deleteUserById() {
+        Long id = 1L;
+        User user = new User(id,"Braian","1234",null);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+
+        userServiceImp.deleteUserById(id);
+
+        verify(userRepository).delete(user);
+
     }
 }
