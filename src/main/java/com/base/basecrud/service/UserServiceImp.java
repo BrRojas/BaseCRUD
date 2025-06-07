15a8bfc3390551fd0025589061e7f00196804417
Braian Rojas <braianrojas95@gmail.com>
2025-06-06 21:19:00 -0300

    feat: agregado manejo de errores global en carpeta exceptions
    - Se agregó clase GlobalExceptionHandler con @ControllerAdvice.
    - Se crearon excepciones personalizadas como ResourceNotFoundException.
    - Estructura lista para capturar y devolver respuestas claras ante errores.

--- src/main/java/com/base/basecrud/service/UserServiceImp.java
+++ src/main/java/com/base/basecrud/service/UserServiceImp.java
@@ -1,12 +1,12 @@
 package com.base.basecrud.service;
 
+import com.base.basecrud.exception.ResourceNotFoundException;
 import com.base.basecrud.model.User;
 import com.base.basecrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImp implements UserService{
@@ -23,14 +23,14 @@ public class UserServiceImp implements UserService{
     @Override
     public User findByID(Long id) {
              User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con ID " + id));
             return user;
     }
 
     @Override
     public User upDateUser(Long id,String mail,String password) {
         User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con ID " + id));
         user.setMail(mail);
         user.setPassword(password);
 
@@ -45,7 +45,7 @@ public class UserServiceImp implements UserService{
     @Override
     public void deleteUserById(Long id) {
        User user = userRepository.findById(id)
-               .orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID " + id));
+               .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con ID " + id));
         userRepository.delete(user);
     }
 
